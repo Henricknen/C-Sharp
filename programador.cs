c3dafa5a013c0f1e725eade7d44c7c5dfbe43257
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-03-14 01:32:54 -0300

    proprietario_deste_github_que_contem_este_repositorio_de_C#_e_outros...

---
+++ programador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace programacaoCompleta
+{
+    class Primeiro_programa
+    {
+        private static void Main()
+        {
+
+            Console.Write("Programador...\n");
+            Console.Write("Luis henrique silva ferreira");
+
+        }
+    }
+}
