68176d6d346f21c13333a72be8f069ad235cb456
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-03-14 09:51:50 -0300

    faz_a_impressao_da_menssagem_e_paramentro_passado_pelo_usuario

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;       /* biblioteca de entrada e saida padrao*/
+
+namespace C_        /* organizaco, onde vai ser aramazenado todos objetos, classes, elementos do pprograma etc */
+{
+    class Program       /* classe Program */
+    {
+        static void Main(string[] args)     /* metodo principal, com argumentos de entrada */
+        {
+            Console.WriteLine("Linguagem orientada a objetos");
+            Console.WriteLine("C#");
+            if(args.GetLength(0) > 0) {
+                Console.Write(args.GetValue(0));
+            }
+        }
+    }
+}
