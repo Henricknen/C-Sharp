9dcf25ff683834f0c001d4c2cffa9219479cee2a
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-04-06 15:15:09 -0300

    ultilizando_constantes_:declararando_e_atribuindo_um_valor_inicial_a_elas

---
+++ constantes.cs
@@ -0,0 +1,9 @@
+using System;
+class constante {
+    static void Main() {
+        const string luis_henrique = "Programador full satck";       // defiinção que isso não é uma variavel e sim uma constante
+        const int idade = 30;
+
+        Console.WriteLine("Luis henrique é um {0}\n e em 2022 possui {1} anos", luis_henrique, idade);
+    }
+}
