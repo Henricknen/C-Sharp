63288a037a6f9bb7f2bc7e78bafc59122a7d2272
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-05-23 23:24:56 -0300

    fazendo_conversoes_de_tipo_implicita_automatica_e_explicita

---
+++ typecast.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace typecast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 10;
+            float n2 = n1;      /* conversão implicita */
+
+            float n3 = 9.7f;
+            int n4 = (int)n3;   /* type cast fanzendo a conversão */
+
+            int vInt = 8;
+            long vLong = vInt;
+
+            int Int = 7;
+            short vShort = (short)Int;    /* type cast convertendo 'int' em 'short' */
+
+            Console.WriteLine(n2);
+            //Console.ReadLine();
+
+            Console.WriteLine(n4);
+            //Console.ReadLine();
+
+            Console.WriteLine(vLong);
+            //Console.ReadKey();
+
+            Console.WriteLine(vShort);
+
+
+
+
+        }
+    }
+}
