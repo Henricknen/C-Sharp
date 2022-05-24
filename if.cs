d858aaa04c4adc06845d523bbe0a44dda56f629f
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-05-24 00:10:10 -0300

    fazendo_teste_condicional_com_comando_'if'

---
+++ if.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace se
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, n4, res;
+            res = n1 = n2 = n3 = n4 = 0;      /* inicializando todas variaveis de uma vez */
+            string resultado = "Reprovado";
+
+
+            Console.Write("Digite a primeira nota: ");
+            n1 = int.Parse(Console.ReadLine());        /* aramzenado a primeira nota 'n1'e fazendo a conversão pra 'int' */
+
+            Console.Write("Digite a segunda nota: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a terçeira nota: ");
+            n3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a quarta nota: ");
+            n4 = int.Parse(Console.ReadLine());
+
+            res = n1 + n2 + n3 + n4;
+
+            if((res) >= 60)
+            {
+                resultado = "Aprovado";
+            }
+
+            Console.WriteLine("Nota: {0} - Resultado:{1}", res, resultado);
+        }
+    }
+
+}
