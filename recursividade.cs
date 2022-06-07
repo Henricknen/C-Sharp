1c2f075286cf48bf9294edc3137a8fe58b7a53d8
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-06-07 00:22:42 -0300

    recursividade_quando_um_metodo_chama_a_si_proprio_um_bom_exemplo_e_um_fatorial

---
+++ recursividade.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Calc
+    {
+        public int soma(params int[]n)          /* sobrecarga de metodos params p */
+        {
+            int s = 0;
+            for(int i = 0; i < n.Length; i++)
+            {
+                s += n[i];
+            }
+            return s;
+        }
+
+        public double soma(params double[]n)
+        {
+            double s = 0;
+            for(int i = 0; i < n.Length; i++)
+            {
+                s += n[i];
+            }
+            return s;
+        }
+    
+    
+                                    /* fatorial */
+        public int fat(int n)        /* função fatorial */
+        {
+            int res;
+            if(n <= 1)
+            {
+                res = 1;        /* se n é menor ou igual a 1, o retorno será 1 */
+            } else
+            {
+                res = n * fat(n - 1);       /* se for maior multiplica ele menos 1 sucessivamente */
+            }
+            return res;
+        }
+
+   
+        static void Main(string[] args)
+        {
+
+            Calc calc = new Calc();
+
+            var res = calc.fat(5);      /* mostrará o fatorial de 5 */
+
+            Console.WriteLine(res);         /* fará o print na tela com o resultado */
+
+            Console.ReadLine();
+        }
+    
+    }
