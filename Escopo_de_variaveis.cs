fcfdf924cc81e766f0545be30b9c6111642af399
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-16 01:09:46 -0300

    escopo_de_variaveis_e_basicamente_onde_as_variaveis_declaradas_sao_visiveis_dentro_do_programa_'variavel_local'_e_declarada_dentro_do_metodo_e_'variavel_global'_e_criada_antes_da_declaracao_de_qualquer_metodo

---
+++ Escopo_de_variaveis.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Escopo_de_variaveis {
+
+    static int num = 10;       // variaveis globais criada na 'classe' antes da declaração de qualquer método
+    static void Main() {
+        int num2 = 5;    // variável 'num' declarada dentro do método principal 'Main' então está variável tem este 'escopo' (area de atuação)
+        Console.WriteLine(num2 + ": é uma variável local e " + num + ": é uma variável global" );     // vai fazer a impressão da variável global 'num'
+    }
+}
