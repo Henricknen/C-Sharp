ab9725f13f48608667af266e5c060e1bc8862c6e
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-19 00:44:45 -0300

    uma_vez_atribuido_o_valor_de_uma_'constantes'_este_valor_nao_podera_ser_modificado_ao_longo_do_programa

---
+++ Constante.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Constante {
+    static void Main() {
+        const string luis_henrique = "Programador full satck";       // definção de constante
+        const int idade = 31;
+
+        Console.WriteLine("Luis henrique é um {0}\n e em 2023 possui {1} anos", luis_henrique, idade);
+    }
+}
