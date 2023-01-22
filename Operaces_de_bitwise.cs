7912e24c1387ef98d11090419b9c25fbbe044278
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-21 22:50:26 -0300

    'bitwise'sao_operacoes_simples_de_deslocamento_de_bits_que_em_resumo_dobram_o_valor_da_variavel_ou_diminuem_pela_metade

---
+++ Operaces_de_bitwise.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Operaces_de_bitwise{
+    static void Main() {
+        int num = 10;
+        int num1 = 30;
+
+        num = num << 1;     // '<<' é um sinal de bitwise que desloca para a esquerda, que duplicará o valor de 'num'
+        num1 = num1 >> 1;         // se o bitwise for a direita '>>'  reduzirá o valor de 'num1' pela metade
+
+        Console.WriteLine(num);
+        Console.WriteLine(num1);
+    }
+}
