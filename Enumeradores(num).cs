cca2e4ac6ba3b2e59390b96aabf44986ed0fc6ed
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-22 23:47:15 -0300

    definindo_um_tipo_com_valores_personalizados_pode_definir_os_valores_que_poderao_ser_atribuidos_aos_elementos_do_tipo_'enum'_que_criar

---
+++ Enumeradores(num).cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeradores_num_
+{
+    internal class Program
+    {
+                //indice:   0		  1		  2		 3		 4		5		6
+        enum DiasSemana { Domigo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado };        /* tipo do Enumerador */
+        static void Main(string[] args)
+        {
+            DiasSemana ds = DiasSemana.Domigo;      /* dentro de 'ds' tem-se o valor relaçionado a Domigo */
+
+            DiasSemana dsi = (DiasSemana)3;      /* ira imprimir o dia que se encontra no indice 3*/
+
+            int dsc = (int)DiasSemana.Sexta;
+
+            Console.WriteLine(ds);
+            //Console.ReadKey();      
+
+            Console.WriteLine(dsi);
+            //Console.ReadLine();
+
+            Console.WriteLine(dsc);
+            Console.ReadKey();      /* se usa 'Console.ReadKey()' para dar uma pausa na execução do programa e usuario ver a impressão */
+
+
+        }
+    }
+}
