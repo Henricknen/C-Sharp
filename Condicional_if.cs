7bda2a6e47902ad4f65624ed23e1125a003744e0
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-24 22:33:08 -0300

    comando_'IF'_realiza_um_teste_condicional_e_dependendo_do_resultado_deste_teste_se_for_'verdadeiro'_ou_'falso'_executa_blocos_de_comandos_diferentes

---
+++ Condicional_if.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Condicional_if {
+    static void Main() {
+
+        int n1, n2, n3, n4, res;
+        res = n1 = n2 =n3 =n4 = 0;      // variáveis iniçializadas em 0
+        string resultado = "Reprovado";
+
+        Console.Write("Digite a primeira nota: ");      // lendo um valor do teclado e armazenando na variável 'n1'
+        n1 = int.Parse(Console.ReadLine());         // e fazendo uma operação de cast com 'int.Parse' para converter a 'string' em 'int' para ser armazenada como 'int' na variável
+
+        Console.Write("Digite a segunda nota: ");
+        n2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a terçeira nota: ");
+        n3 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a quarta nota: ");
+        n4 = int.Parse(Console.ReadLine());
+
+        res = n1 + n2 + n3 + n4;         
+
+        if(res >= 60) {
+            resultado = "Aprovado";     // mudando o valor da variável caso a condição 'if' seja sastisfeita
+        }
+
+        Console.WriteLine("Nota: {0} - Resultado: {1}", res, resultado);      // imprimindo a menssagem final
+    }
+}
