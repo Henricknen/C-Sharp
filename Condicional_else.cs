617ba5120f3799b6dac608c6765ee2fde091f318
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-24 23:37:21 -0300

    'ELSE'_e_o_caso_contrario_de_'IF'_caso_o_teste_do_'IF'_seja_verdadeiro_seu_bloco_de_comandos_e_executado_se_caso_seja_falso_o_bloco_de_comandos_a_ser_executado_sera_o_do_'ELSE'

---
+++ Condicional_else.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class condicional_if_else {
+    static void Main() {
+
+        int n1, n2, n3, n4, res;
+        res = n1 = n2 =n3 =n4 = 0;      // variáveis iniçializadas em 0
+        string resultado;
+
+        Console.Write("Digite a primeira nota: ");      // lendo um valor do teclado e armazenando na variável 'n1'
+        n1 = int.Parse(Console.ReadLine());         // e fazendo uma operação de cast com 'int.Parse' para converter a 'string' em 'int' para ser armazenada como 'int' na variável
+
+        Console.Write("Digite a segunda nota: ");
+        n2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a terçeira nota: ");
+        n3 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a quarta nota: ");
+        n4 = int.Parse(Console.ReadLine());
+
+        res = n1 + n2 + n3 + n4;         
+
+        if(res < 40) {
+            resultado = "Reprovado";
+        } else if(res < 60) {
+            resultado = "Recuperação";
+        } else {
+            resultado = "Aprovado";
+        }
+
+        Console.WriteLine("Nota: {0} - Resultado: {1}", res, resultado);      // imprimindo a menssagem final
+    }
+}
