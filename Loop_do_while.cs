00453c1042fed8c9ae327d3b21e66bc6df36402f
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-28 16:50:32 -0300

    loop_'do_while'_executa_o_bloco_de_comandos_e_so_depois_ele_verifica_a_condicao_de_execucao_garantindo_assim_que_pelo_menos_uma_vez_os_comandos_serao_executados

---
+++ Loop_do_while.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Loop_do_while {
+    static void Main() {
+
+        int num = 5;
+        do {        // 'do while' executa os comandos primeiro e depois testa
+            Console.WriteLine("Codificando C#");
+        } while(num < 5);
+
+        string senha = "123";
+        string senhauser;
+        int tentativas = 0;
+
+        do{
+            Console.Clear();        // limpa a tela
+            Console.WriteLine("Digite a senha");
+            senhauser = Console.ReadLine();
+            tentativas++;
+        } while(senha != senhauser);
+
+        Console.Clear();
+        Console.WriteLine("Senha correta, tentativas:{0}", tentativas);
+    }
+}
