b26282e337cda92a8978ecfb59b392ba0eea3b0e
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-28 00:43:06 -0300

    'loop_while'_e_indicado_quando_nao_sabe_ao_certo_quantas_vezes_o_bloco_de_comandos_ira_ser_executado

---
+++ Loop_while.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Loop_while {
+    static void Main() {
+        
+        int i = 0;
+        while(i < 10) {     // enquanto
+            Console.WriteLine("{0} Luis henrique S", i);
+            i++;
+        }
+        Console.WriteLine("Fim");
+    }
+}
