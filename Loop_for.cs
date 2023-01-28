a08c27050afabae4e3eba88dcd48658e18b999bf
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-27 21:09:02 -0300

    'loop_for'_executa_um_bloco_de_comandos_um_numero_determinado_ou_indeterminado_de_vezes_da_para_determina_quantas_vezes_os_comandos_serao_repetidos

---
+++ Loop_for.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Loop_for {
+    static void Main() {
+
+        for(int num = 0; num < 10; num++) {
+            Console.WriteLine("Codificando C#");
+            Console.WriteLine("Indiçe de número: {0}", num);
+        }
+    }
+}
