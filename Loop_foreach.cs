ecc85b0672605b97829be997a81be098652d07ee
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-28 23:34:17 -0300

    'loop_foreach'_e_indicado_quando_for_necessario_ler_os_elementos_de_uma_colecao_como_um_array/vetor

---
+++ Loop_foreach.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Loop_foreach {
+    static void Main() {
+
+        int[] num = new int[5]{11, 22, 33, 44, 55};       // coleção 'num'
+
+        foreach(int n in num) {        // 'n' é uma variável recebe cada elemento da coleção 'num'
+            Console.WriteLine(n);
+        }
+    }
+}
