4d5a12a40a6cc37f05ef41312e0e22477f34fc46
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-29 20:07:27 -0300

    fazendo_passagem_de_valor_para_metodo_'Main'_fazendo_passagem_por_'valor'_e_por_'referencia'

---
+++ Passagem_por_valor_epor_referencia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class passagem_por_valor_epor_referencia {
+    static void Main() {
+        int num1 = 50;   // variável local 'num' iniçializado com valor 10
+        dobrar1(ref num1);        // chamando método 'dobrar' com variável 'num' e o operador 'ref' indica que está sendo feita uma passagem por referênçia
+        Console.WriteLine(num1);
+
+        int num2 = 75;
+        dobrar2(num2);      // o valor da variável não será alterado pois está recebendo o valor por 'ref' referênçia
+        Console.WriteLine(num2);
+    }
+
+    static void dobrar1(ref int valor) {     // método dobrar1 ultilizando 'ref' faz uma passagem por referênçia
+        valor *= 2;
+    }
+
+    static void dobrar2(int valor) {        // método dobrar2 faz uma passagem por valor
+        valor *= 2;
+    }
+}
