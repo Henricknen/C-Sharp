eac77107f6d0bb4557c10282150be7bb0f28fadc
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-30 00:35:53 -0300

    o_argumento_'out'_permite_que_o_metodo_retorne_mais_de_um_valor_de_saida

---
+++ Argumento_out.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Argumento_out {
+    static void Main() {
+        int divid, divis, quoc, rest;
+        divid = 10;
+        divis = 3;
+        quoc = divide(divid, divis, out rest);
+
+        Console.WriteLine("{0}/{1}:quociente={2} e resto={3}", divid, divis, quoc, rest);
+    }
+
+    static int divide(int dividendo, int divisor, out int resto) {     // método 'divide' recebe o 'dividendo' e 'divisor' e ele retornará o 'quociente' da divisão e o 'resto'
+        int quociente;
+        quociente = dividendo / divisor;
+        resto = dividendo % divisor;
+        return quociente;
+    }
+}
+
