6edc2447c0bfc092721db57eb818fe779235dec6
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-30 14:12:20 -0300

    argumento_'params'_e_um_array_que_permite_a_entrada_de_0_ou_mais_argumentos_para_dentro_de_uma_funcao

---
+++ Argumento_params.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Argumento_params {
+    static void Main() {
+        soma(6, 9, 56, 3,12);     // chamando a unção soma
+    }
+
+    static void soma(params int[]n) {     // método de soma com com 'params'
+        int res = 0;
+
+        if(n.Length < 1) {
+            Console.WriteLine("Não existe valores para serem somados");
+        } else if(n.Length < 2) {
+            Console.WriteLine("Valores insulficientes para soma {0}", n[0]);
+        } else {
+            for(int i = 0; i < n.Length; i++ ) {
+                res += n[i];
+            }
+            Console.WriteLine("A soma dos valores é: {0}", res);
+        }
+    }
+}
