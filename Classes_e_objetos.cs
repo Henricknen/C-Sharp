9c24db6cb6562aabbba30f45f4c80e42dc94aac9
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-30 15:19:38 -0300

    'classes'_contem_membros_que_sao_as_propriedades_'variaveis'_e_os_metodos_que_sao_as_'funcoes'_da_classe_elas_dao_origem_aos_'objetos'_por_meio_da_operacao_de_instanciacao_usando_o_operador_'new'

---
+++ Classes_e_objetos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Jogador {// criação de nova classe e uma boa prática iniçiar nomes das classes  com letras maiusculas
+    public int energia = 100;       // propriedade 'int' energia
+    public bool vivo = true;
+}
+class Classes_e_objetos {
+    static void Main() {
+        Jogador j1 = new Jogador();     // instançiando objeto da classe 'Jogador' 'new' reseva um espaço na memória RAM para o objeto intançiado
+        Jogador j2 = new Jogador();
+        Jogador j3 = new Jogador();
+
+        j1.energia = 50;        // 'energia' por ser uma propeiedade pública é possível  fazer alteração
+        Console.WriteLine("Energia do Jogador1: {0}", j1.energia);
+        Console.WriteLine("Energia do Jogador2: {0}", j2.energia);      // cada 'Jogador' funcionará de forma indepedente
+    }
+}
