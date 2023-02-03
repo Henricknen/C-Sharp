176d9baee8c18cd7554975280ac0632e4503a47c
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-03 00:26:17 -0300

    a_ordem_de_execucao_dos_construtores_quando_se_tem_multiplas_herancas_sera_da_classe_'base'_para_a_classe_'derivada'

---
+++ Ordem_de_execucao_dos_construtores.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Base {        // classe 'Base'
+    public Base() {     // construtor
+        Console.WriteLine("Construtor da classe Base");
+    }
+}
+
+class Derivada1: Base {     // classe 'Derivada1' herda a classe "Base'
+    public Derivada1() {        // construtor
+        Console.WriteLine("Construtor da classe Derivada 1");
+    }
+}
+
+class Derivada2: Derivada1 {        // classe 'Derivada2' herda a classe "Derivada1'
+    public Derivada2() {        // construtor
+        Console.WriteLine("Construtor da classe Derivada 2");
+    }
+}
+
+class Ordem_de_execucao_dos_construtores {
+    static void Main() {
+        Derivada2 derivada2 = new Derivada2();      // criação do objeto 'derivada2'
+    }
+}
