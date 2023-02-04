fa6bdc0cd2289e244e9dfb878bfa8b7a054d273f
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-03 22:59:13 -0300

    classe_'abastrada'_serve_como_base_para_outras_classes_que_vao_herda

---
+++ Classes_e_metodos_abstratos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+abstract class Veiculo {     // classe base 'abstrata' só serve para ser herdada
+    protected int velMaxima;
+    protected int velAtual;
+    protected bool ligado;
+    public Veiculo() {      // construtor
+        ligado = false;
+        velAtual = 0;
+    }
+    public void setLigado(bool ligado) {        // método para ligar o Veiculo
+        this.ligado = ligado;       // o método 'setLigado' não é abstrato intão é obrigatório implementar a sua funçionalidade
+    }
+    public int getVelAtual() {
+        return velAtual;
+    }
+    abstract public void aceleracao(int mult);     // método abastrato
+}
+class Carro:Veiculo {        // classe 'Carro' herda classe 'Veiculo'
+    public Carro() {        // construtor da classe 'Carro'
+        velMaxima = 120;
+    }
+    override public void aceleracao(int mult){      // 'override' sobreescreve o método
+        velAtual += 10 * mult;
+    }
+}
+class Classes_e_metodos_abstratos {
+    static void Main() {
+        Carro carro1 = new Carro();
+
+        carro1.aceleracao(1);       // aumentando a açeleração em 10
+        carro1.aceleracao(1);
+        carro1.aceleracao(-1);          // diminuindo a açeleração em 10
+
+        Console.WriteLine(carro1.getVelAtual());
+    }
+}
