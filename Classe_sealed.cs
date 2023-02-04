0faad96e80e9e8221557202f1bf8ecc54c500a67
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-04 19:19:39 -0300

    quando_e_o_modificador_da_classe_e_definido_como_'sealed'_impede_que_outras_classes_herdem_dela

---
+++ Classe_sealed.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+sealed class Veiculo {        // os membros de uma classe 'sealed' não pode ser herdada
+
+}
+
+class Carro: Veiculo {          // a Classe 'Carro' não herdará 'Veiculo'
+
+}
+
+class Classe_sealed {
+    static void Main() {
+        Carro c1 = new Carro();     // instançiando objeto da classe'Carro'
+    }
+}
