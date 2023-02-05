77db24d0e875617eed9d1abb36594c857f35ac86
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-05 15:10:45 -0300

    a_estrutura_'struct'_e_um_tipo_que_permite_o_armazenanamento_de_diferentes_tipos_de_dados

---
+++ Struct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+struct Carro {      // estrutura 'Carro'
+    public string marca;
+    public string modelo;
+    public string cor;
+    public Carro(string marca, string modelo, string cor) {       // construtor que atribui valores aos elementos 'marca, modelo e cor'
+        this.marca = marca;
+        this.modelo = modelo;       // iniçializando os 'membros'
+        this.cor = cor;
+    }
+
+    public void info() {        // classe que retornará os valores, informações dos 'membros'
+        Console.WriteLine("Marca.: {0}", this.marca);
+        Console.WriteLine("Modelo: {0}", this.modelo);
+        Console.WriteLine("Cor...: {0}", this.cor);
+    }
+}
+class Struct {
+    static void Main() {
+
+        Carro c1 = new Carro("Honda", "HRV", "Prata");       // chamando o contrutor de atribuição atraves do 'new'
+        Carro c2 = new Carro("VM", "Golf", "Preto");
+
+        // Carro c1;       // aqui já está sendo usada a 'struct' 'Carro'
+
+        // c1.marca = "VW";
+        // c1.modelo = "Golf";
+        // c1.cor = "Preto";
+
+        c1.info();      // chamando o método 'info' 
+        c2.info();
+    }
+}
