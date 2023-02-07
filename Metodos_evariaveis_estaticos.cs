591331794afcd4ea3c7494d1abf5dba31235c19d
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-07 00:57:28 -0300

    metodos_e_variaveis_estaticas_e_basicamente_quando_se_declara_um_metodo_como_'static'_e_pode_acessa-lo_sem_ter_que_instanciar_um_objeto_da_classe

---
+++ Metodos_evariaveis_estaticos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Mat {     // classe 'Mat'
+    public static double pi = 3.14;     // propriedade 'pi'
+
+    public static int dobro(int n) {        // método 'dobro' que retornará o dobro de um valor
+        return n * 2;
+    }
+}
+
+class Metodos_evariaveis_estaticos {
+    static void Main() {
+
+        double vpi = Mat.pi;        // variável 'vpi' chma a propriedade 'pi' da classe 'Mat'
+        int num = 10;
+
+        Console.WriteLine("O valor de PI é: {0}", Mat.pi);      // verificando o 'dobro' da variável num
+        Console.WriteLine("O dobro de da variável é: {0}", Mat.dobro(num));      // verificando o 'dobro' da variável num
+    }
+}
