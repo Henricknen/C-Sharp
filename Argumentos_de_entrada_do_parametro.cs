cd4c67cf7c990545243d568689b2bbb1d666225d
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-07 15:36:04 -0300

    codigo_basico_que_passa_valores_como_argumentos_para_o_programa_no_momento_da_'chamada'_da_execucao_e_faz_a_leitura_destes_argumentos_passados

---
+++ Argumentos_de_entrada_do_parametro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Argumentos_de_entrada_do_parametro {
+    static void Main(string[] args) {        // 'string[] args' cria uma lista de entrada de argumentos
+
+        int res = 0;
+
+        if(args.Length > 0) {       // verificando se foi passado algum parâmetro
+            Console.WriteLine("Qtde de argumentos {0}", args.Length);       // faz o print da quantidade de argumentos passado
+            for(int i = 0; i < args.Length; i++) {
+                res += Int32.Parse(args[i]);        // 'res' reçebe a soma da posição'i' de args
+            }
+            Console.WriteLine("Soma: {0}", res);        // imprimido a soma
+        } else {
+            Console.WriteLine("Não foram passados argumentos");
+        }
+
+    }
+}
