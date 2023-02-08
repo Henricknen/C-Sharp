42e0ce4cda44bb3d12efeada1773460d12c61f45
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-08 00:52:37 -0300

    excecao_e_um_erro_que_ocorre_na_execucao_do_programa_para_nao_deixar_a_execucao_mostra_esse_erro_a_operacao_que_ocasionara_tal_erro_e_adicionada_dentro_de_um_bloco_de_tratamento_chamado_'try_catch'

---
+++ Try_catch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Try_catch {
+    static void Main() {
+
+        int n1, n2, res;        // criação de variáveis do tipo 'int'
+        res = n1 = n2 = 0;  // iniçializando todas variáveis de uma vez
+
+        n1 = 10;
+        n2 = 0;     // atribuindo valores as 'variáveis
+
+        try {       // bloco 'try'é quem executa as operações
+            res = n1 / n2;      // operação
+            Console.WriteLine("{0}/{1} = {2}", n1, n2, res);        // se apresentar 'erro' não será apresentado na tela, pois ao executar o 'erro' é interrompida a operação
+        } catch(Exception e) {       // caso haja algum erro na operação, o erro será tratado e entrará no bloco 'catch', 'Exception e' faz a interçiptação e emite a menssagem de erro
+            Console.WriteLine("ERROR:{0}", e.Message);      // '.Message' mostra a menssagem do erro
+            Console.WriteLine("Ex:{0}", e);     // 'e' mostra o tipo e a excessão espeçifica
+        }
+
+        
+    }
+}
