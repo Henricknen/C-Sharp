e763083c0e55760c3ac6bbcfb2604e99d63afdc3
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-09 00:55:38 -0300

    colecao_'LinkedList'_e_uma_lista_'duplamente_encadeada'_onde_cada_elemento_e_um_no_que_tem_um_link_para_o_elemento_anterior_e_posterior_da_lista

---
+++ Colecao_LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;       // fazendo a inclusão de biblioteca e ultilizando 'Generic' para trabalha com as coleções genéricas
+
+class Colecao_LinkedList {
+    static void Main() {
+
+        LinkedList<string> transp = new LinkedList<string>();      // criando 'LinkedList'
+
+        transp.AddFirst("Carro");     // adiçionando elementos no iniçio da lista com método 'AddFirst'
+        transp.AddFirst("Avião");
+        transp.AddFirst("Moto");
+        transp.AddFirst("Barco");
+
+        transp.AddLast("Navio");        // 'AddLast' adiçiona no fim da lista
+
+        LinkedListNode<string>no;       // criação do 'LinkedListNode' do tipo 'string' chamado 'no'
+        no = transp.FindLast("Avião");       // ponto de referênçia 'no', 'FindLast' enconta o ultimo 'Avião'
+        transp.AddAfter(no, "Biçicleta");       // método 'AddAfter' adiçionará biçicleta 'após' o 'no'
+
+        no = transp.FindLast("Avião");
+        transp.AddBefore(no, "Patinete");       // método 'Patinete' adiçionará Patinete 'antes' do 'no'
+
+        // transp.Clear();     // método 'Clear' remove todos elementos
+
+        if(transp.Find("Moto") == null) {        // método 'Find' verifica se o elemento está na lista
+            Console.WriteLine("Não encontrado");
+        } else {
+            Console.WriteLine("Elemento encontrado");
+        }
+
+        // transp.Remove("Barco");      // 'Remove' remove um elemento
+        transp.RemoveFirst();       // remove o primeiro elemento
+        transp.RemoveLast();    // remove o último
+
+        foreach(string t in transp) {// percorrendo a lista e imprimindo os elementos com 'foreach'
+            Console.WriteLine("Transporte: {0}", t);
+        }
+
+    }
+}
