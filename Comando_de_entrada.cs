55421b012adabe7380642d020836f6f51f6305f6
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-15 14:13:31 -0300

    criando_variaveis_e_fazendo_a_leitura_de_dados_que_usuario_digitar

---
+++ Comando_de_entrada.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class  Comando_de_entrada {
+    static void Main() {
+
+        string nome = "";        // variável 'nome' vazia
+
+        Console.WriteLine("Programa que efetua leitura de dados do usuário");
+        Console.WriteLine("Qual é o seu nome?");
+        Console.Write("Digite seu nome:");
+        nome = Console.ReadLine();     // 'ReadLine' vai armazenar na variável 'nome' o que o usuário digitou e aguarda o usuário pressionar enter para fazer o print
+        // Console.WriteLine(nome + " Seja bem vindo...");
+        Console.WriteLine(" Bem vindo ao repositório de C# " + nome);        // outra maneira de ultilizar o comando 'WriteLine' que trocará '{0}' pelo conteúdo da variável 'nome'
+
+    }
+}
