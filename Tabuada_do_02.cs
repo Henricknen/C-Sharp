ad10666883f01214b3c99df318467119ab194c74
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-15 08:47:54 -0300

    criacao_de_tabuada_do_numero_2_em_liguagem_'C#'

---
+++ Tabuada_do_02.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Tabuada_do_02 {
+    static void Main() {
+        Console.WriteLine("Tabuada do 2");
+        // Console.Write("2 x 1 = ");
+        // Console.WriteLine(2 * 1);       // linha de comando
+
+        Console.WriteLine("2 x 1 = " + (2 * 1));
+        Console.WriteLine("2 x 2 = " + (2 * 2));
+        Console.WriteLine("2 x 3 = " + (2 * 3));
+        Console.WriteLine("2 x 4 = " + (2 * 4));
+        Console.WriteLine("2 x 5 = " + (2 * 5));
+        Console.WriteLine("2 x 6 = " + (2 * 6));
+        Console.WriteLine("2 x 7 = " + (2 * 7));
+        Console.WriteLine("2 x 8 = " + (2 * 8));
+        Console.WriteLine("2 x 9 = " + (2 * 9));
+        Console.WriteLine("2 x 10 = " + (2 * 10));
+
+        Console.ReadKey();      // ultilizando 'Console.ReadKey' ele vai fazr a leitura de qualquer tecla pressionada muda o valor da mesma e continyar a execução do programa
+    }
+}
