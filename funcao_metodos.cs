305901148923594354b39b33d382ae3506f6dc9c
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-16 11:23:46 -0300

    criando_funcao_que_mostra_data_atual_funcao_e_um_'pedacinho_de_programa'_que_quando_efetuada_a_chamada_deste_programa_ele_executa_uma_acao_devolve_um_valor

---
+++ funcao_metodos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncaoMetodos
+{
+    internal class Program { 
+
+        public static void ExibirData() {        // modificador de acesso 'public' todos que tiverem acesso a ele pode usar 'static' diz que o pedaçinho de programa pertençe a classe tipo 'void' executa uma ação e não devolve nada
+
+            string data = DateTime.Now.Date.ToString();        // convertendo para texto 'ToString' uma data "date' obtidida pelo comando 'Now' que devolve um objeto do tipo 'date'
+        Console.WriteLine(data);        // escrevendo a data na tela
+
+        }
+        static void Main(string[] args) {
+
+
+            Console.WriteLine("Trabalhando com função");     // o comando 'WriteLine' é uma função que recebe um parâmetro e executa a ação de mostrar algo na tela
+            ExibirData();       // chamando a função 'ExibirData'
+            Console.ReadKey();      // para visualizar o que vai aconteçer
+
+        }
+    }
+}
