20e9f0c930d14b662af81221177588c3549b9b5e
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-18 20:15:44 -0300

    criando_'menu'_que_fara_parte_de_um_editor_de_texto_criado_em_'C#_no_modo_console'

---
+++ Editor_de_texto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Editor_de_texto {
+
+    static int ExibirMenu() {       // função que vai criar um menu
+        Console.Clear();
+        Console.WriteLine("...Editor de Texto...");
+        Console.WriteLine("Abrir um arquivo (1)");
+        Console.WriteLine("Exibir texto do arquivo (2)");
+        Console.WriteLine("Gravar um texto no arquivo (3)");
+        Console.WriteLine("Gravar novo texto no arquivo(4)");
+        Console.WriteLine("Sair(5)");
+        Console.Write("Opção: ");      
+        int op = Convert.ToInt32(Console.ReadLine());       // convertendo a leitura do usúario
+        return op;
+    }
+
+    static void Main(string[] args) {
+        int op = 0;
+        while (op != 5) {
+            op = ExibirMenu();
+        }
+    }
+}
