b2055d5757e46f9b8b5540bade6fefa3f78e8136
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-21 18:13:36 -0300

    criando_um_conversor_de_temperatura_que_converte_'Celsius'_em_'Fahrenheit'

---
+++ Conversor_de_temperatura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Conversor_de_temperatura {
+    static void Main() {
+        double celsius, fahrenheit;
+
+        Console.Write("Digite a temperatura em graus Celsius: ");
+        celsius = double.Parse(Console.ReadLine());
+
+        fahrenheit = celsius * 1.8 + 32;
+
+        Console.WriteLine($"A temperatura em Fahrenheit é: {fahrenheit}");
+
+        // Console.ReadLine();
+    }
+}
