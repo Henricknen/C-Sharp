fd81208de932fa30833f3610437064ecb0a3b573
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-22 20:11:21 -0300

    funcao_recursiva_chamada_potencia(x, y)_que_retorna_a_base_'x'_elevado_ao_expoente_'y'

---
+++ Potencia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Potencia {
+    public static double potencia(double x, int y) {
+        if (y == 0) {
+            return 1;
+        }
+        else if (y == 1) {
+            return x;
+        }
+        else if (y < 0) {
+            return 1 / potencia(x, -y);
+        }
+        else if (y % 2 == 0) {
+            return potencia(x*x, y/2);
+        }
+        else {
+            return x * potencia(x*x, (y-1)/2);
+        }
+    }
+
+    public static void Main() {
+        double resultado = potencia(2, 3);
+        Console.WriteLine(resultado);
+    }
+}
