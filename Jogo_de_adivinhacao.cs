bb2716a8fe3a07699b8ec7a1683c78fb9164e864
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-22 00:12:06 -0300

    criando_jogo_de_adivinhacao_de_numeros_de_1_a_100_ultilizando_funcao_'random'_que_faz_o_sorteio_de_numeros_aleatorios

---
+++ Jogo_de_adivinhacao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Jogo_de_adivinhacao {
+    static void Main() {
+        Random random = new Random();       // 'Random' gera numeros aleatorios
+        int numero = random.Next(1, 101);
+        int tentativas = 0;
+        int palpite;
+
+        Console.WriteLine("Bem-vindo ao jogo de adivinhação!");
+        Console.WriteLine("Estou pensando em um número entre 1 e 100.");
+        Console.WriteLine("Tente adivinhar qual é o número.");
+
+        do {
+            Console.Write("Digite o seu palpite: ");
+            palpite = int.Parse(Console.ReadLine());
+            tentativas++;
+
+            if (palpite < numero) {
+                Console.WriteLine("O número que estou pensando é maior.");
+            } else if (palpite > numero) {
+                Console.WriteLine("O número que estou pensando é menor.");
+            }
+        } while (palpite != numero);
+
+        Console.WriteLine($"Parabéns, você acertou em {tentativas} tentativas!");
+
+    }
+}
