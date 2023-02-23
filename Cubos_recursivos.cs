61d219147e2f2615bd0e5e1eb67f0dd0fe53d988
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-22 23:03:45 -0300

    criacao_de_uma_repeticao_recursiva_somente_do_for

---
+++ Cubos_recursivos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Cubos_recursivos {
+    public static void Cubos(int n) {
+        CubosRecursivo(n, 1);
+    }
+
+    public static void CubosRecursivo(int n, int i) {
+        if (i <= n) {
+            Console.WriteLine(i * i * i);
+            CubosRecursivo(n, i + 1);
+        }
+    }
+
+    public static void Main() {
+        Cubos(5); 
+    }
+}
