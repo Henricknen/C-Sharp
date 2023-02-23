4aa3fa05eea6289b327cf7df2a7ffa0cc802d5b6
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-23 01:32:28 -0300

    implementando_uma_funcao_recursiva_para_determinar_o_maximo_divisor_comum_'mdc'_entre_dois_numeros_inteiros

---
+++ Mdc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Mdc {
+    static void Main(string[] args) {
+        int x = 12;
+        int y = 18;
+        int mdc = MDC(x, y);
+        Console.WriteLine("O MDC de {0} e {1} é: {2}", x, y, mdc);
+    }
+
+    static int MDC(int x, int y) {
+        if (x == y) {
+            return x;
+        }
+        else if (x < y) {
+            return MDC(y, x);
+        }
+        else {
+            return MDC(x - y, y);
+        }
+    }
+}
