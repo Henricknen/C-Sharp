afe9d6592fce758675c3808df94d0f5e46308ac3
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-23 16:41:28 -0300

    criando funcao_recursiva_onde_o_nonagesimo_termo_e_a_soma_dos_dois_termos_anteriores

---
+++ Fibonacci_recursiva.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        for (int i = 0; i <= 10; i++) {     // loop chama a função recursiva
+            Console.WriteLine($"FibRecursive({i}) = {FibRecursive(i)}");
+        }
+    }
+    public static int FibRecursive(int n) {     // função recursiva recebe um parâmetro inteiro
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        else 
+            return FibRecursive(n - 1) + FibRecursive(n - 2);       // chamando função recursiva
+        }
+    }
