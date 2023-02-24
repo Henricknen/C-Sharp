d480ddcc38984b1152d2f89776b5f5ddf7b597d6
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-24 17:07:30 -0300

    criando_funcao_recursiva_que_retorna_um_valor

---
+++ Funcao_retorna_valor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Funcao_retorna_valor {
+    static void Main(string[] args) {
+        int valorRetornado = Recursao(27);
+        Console.WriteLine(valorRetornado);
+    }
+
+    public static int Recursao(int n) {
+        if (n <= 10) {
+            return n * 2;
+        }
+        else {
+            return Recursao(Recursao(n / 3));
+        }
+    }
+}
