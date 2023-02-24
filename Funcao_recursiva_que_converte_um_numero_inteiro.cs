ed6400207aba8a3737574346903de7531f8127e2
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-24 14:21:42 -0300

    criando_funcao_recursiva_que_converte_um_numero_'int'_para_binario_'DecimalToBinary'

---
+++ Funcao_recursiva_que_converte_um_numero_inteiro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main() {        // solicita o número inteiro para o usuário
+        Console.Write("Digite um número inteiro: ");
+        int num = int.Parse(Console.ReadLine());
+
+        string binary = DecimalToBinary(num);       // converte o número para a base binária usando a função recursiva
+
+        Console.WriteLine($"O número {num} em binário é: {binary}");        // exibe o resultado
+    }
+
+    public static string DecimalToBinary(int n) {
+        if (n == 0) {
+            return "0";
+        }
+        else if (n == 1) {
+            return "1";
+        }
+        else {
+            string binary = DecimalToBinary(n / 2);            // chama a função recursivamente para n/2
+            if (n % 2 == 0) {            // verifica se n é par ou ímpar e adiciona 0 ou 1 à string binária
+                return binary + "0";
+            }
+            else {
+                return binary + "1";
+            }
+        }
+    }
+}
