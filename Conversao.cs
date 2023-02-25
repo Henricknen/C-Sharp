9df053df004ba27d6cab83503a0253334b32c634
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-24 23:17:47 -0300

    convertendo_numero_de_base_'decimal'__para_base_'binaria'

---
+++ Conversao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Conversao {
+    static void Main() {
+        Console.Write("Digite um número inteiro em decimal: ");
+        int decimalNumber = int.Parse(Console.ReadLine());
+        string binaryNumber = "";
+
+        while (decimalNumber > 0) {
+            int remainder = decimalNumber % 2;
+            binaryNumber = remainder.ToString() + binaryNumber;
+            decimalNumber = decimalNumber / 2;
+        }
+
+        Console.WriteLine("O número em binário é: " + binaryNumber);
+    }
+}
