80471c5acaf2d5712136d328c3433d6fb4b5c655
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-25 22:08:42 -0300

    ultilizando_metodo_'split'_para_dividir_a_frase_em_um_array_de_palavras_e_'for'_para_percorrer_a_palavra_de_traz_para_frente_adicionando_cada_caractere_na_variavel_'fraseInvertida'

---
+++ Palavras_invertidas.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+
+class Palavras_invertidas {
+    static void Main() {
+        Console.Write("Digite uma frase: ");
+        string frase = Console.ReadLine();
+        string[] palavras = frase.Split(' ');       // método 'Split' para dividir a frase em um array de palavras, considerando o espaço como separador
+        string fraseInvertida = "";     // variável 'fraseInvertida' para armazenar a frase invertida
+        foreach (string palavra in palavras) {      //  'loop foreach' irá percorrer todas as palavras da frase
+            string palavraInvertida = "";       //  variável 'palavraInvertida' armazenará a palavra invertida
+            for (int i = palavra.Length - 1; i >= 0; i--) {
+                palavraInvertida += palavra[i];
+            }
+            fraseInvertida += palavraInvertida + " ";
+        }
+        Console.WriteLine("Frase invertida: " + fraseInvertida.TrimEnd());      // método 'TrimEnd()' remove o espaço em branco no final da frase
+    }
+}
