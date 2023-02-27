5056914fe8aba49c8954a7669828fb2e4ba6e2c8
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-26 22:06:04 -0300

    verificando_se_uma_expressao_e_valida_em_relacao_aos_abre_e_fecha_parenteses_colchetes_e_chaves

---
+++ Verificando_expressao_valida.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Verificando_expressao_valida {
+    static void Main(string[] args) {
+        string expressao = "while (m < (n[8] + o)) { m = m + 1; }";
+        if (VerificarExpressao(expressao)) {
+            Console.WriteLine("Expressão válida!");
+        } else {
+            Console.WriteLine("Expressão inválida!");
+        }
+    }
+
+    static bool VerificarExpressao(string expressao) {
+        
+        Dictionary<char, char> pares = new Dictionary<char, char>();        // criando um dicionário com os pares de caracteres a serem verificados
+        pares.Add('(', ')');
+        pares.Add('[', ']');
+        pares.Add('{', '}');
+        
+        Stack<char> pilha = new Stack<char>();      // criando uma pilha para armazenar os caracteres de abertura
+        // Percorre cada caractere da expressão
+        foreach (char caractere in expressao) {
+            
+            if (pares.ContainsKey(caractere)) {     // se o caractere for um caractere de abertura, adiciona na pilha
+                pilha.Push(caractere);
+            }
+            
+            if (pares.ContainsValue(caractere)) {       // se o caractere for um caractere de fechamento, verifica se é correspondente
+                
+                if (pilha.Count == 0 || pares[pilha.Peek()] != caractere) {     // se a pilha estiver vazia ou o último caractere adicionado não for o correspondente, retorna falso
+                    return false;
+                }
+                
+                pilha.Pop();        // remove o último caractere adicionado da pilha
+            }
+        }
+        
+        return (pilha.Count == 0);      // se a pilha estiver vazia, a expressão é válida
+    }
+}
