6795703709c791b3ae84c9a1fc2ca19121789916
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-28 23:50:53 -0300

    criando_fila_cujo_valores_indicam_prioridade_quanto_maior_o_valor_maior_a_prioridade

---
+++ Fila_prioridade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Fila_prioridade {
+    static void Main(string[] args) {
+
+        int[] filaPrioridade = { 10, 5, 15, 20, 8, 12 };        // criar uma fila com valores de prioridade
+        
+        Console.WriteLine("Fila original:");        // exibir a fila original
+        foreach (int valor in filaPrioridade) {
+            Console.WriteLine(valor);
+        }
+
+        Console.Write("\nDigite um valor divisor: ");        // solicitar o valor divisor
+        int divisor = int.Parse(Console.ReadLine());
+
+        // criar as novas filas
+        int[] filaMaiorPrioridade = new int[filaPrioridade.Length];
+        int[] filaMenorPrioridade = new int[filaPrioridade.Length];
+        int tamanhoFilaMaior = 0;
+        int tamanhoFilaMenor = 0;
+
+        foreach (int valor in filaPrioridade) {     // dividir a fila original nas novas filas
+            if (valor > divisor) {
+                filaMaiorPrioridade[tamanhoFilaMaior++] = valor;
+            } else {
+                filaMenorPrioridade[tamanhoFilaMenor++] = valor;
+            }
+        }
+
+        Console.WriteLine("\nFila de maior prioridade:");       // exibir as novas filas
+        for (int i = 0; i < tamanhoFilaMaior; i++) {
+            Console.WriteLine(filaMaiorPrioridade[i]);
+        }
+
+        Console.WriteLine("\nFila de menor prioridade:");
+        for (int i = 0; i < tamanhoFilaMenor; i++) {
+            Console.WriteLine(filaMenorPrioridade[i]);
+        }
+    }
+}
