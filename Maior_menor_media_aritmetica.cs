b914256cd938a8ac4fb0539acebcf05a13e2bca8
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-03-02 00:37:52 -0300

    programa_que_e_possivel_adicionar_varios_valores_em_uma_'fila'_apos_comando_'break'_programa_imprime_na_tela_maior_menor_valor_e_media_aritmetica_de_todos_valores

---
+++ Maior_menor_media_aritmetica.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Maior_menor_media_aritmetica {
+    static void Main(string[] args) {
+        List<double> fila = new List<double>();     // lista vazia para armazenar os números digitados
+
+        while (true) {
+            Console.Write("Digite um número (ou 'fim' para encerrar): ");
+            string numero = Console.ReadLine();
+            if (numero == "fim") {
+                break;
+            }
+            try {
+                fila.Add(double.Parse(numero));
+            } catch (FormatException) {
+                Console.WriteLine("Valor inválido. Digite um número ou 'fim' para encerrar.");
+            }
+        }
+
+        if (fila.Count == 0) {
+            Console.WriteLine("Nenhum número foi digitado.");
+        } else {
+            double maior = fila.Max();
+            double menor = fila.Min();
+            double media = fila.Average();
+
+            Console.WriteLine("O maior número é: " + maior);
+            Console.WriteLine("O menor número é: " + menor);
+            Console.WriteLine("A média aritmética dos números é: " + media);
+        }
+    }
+}
