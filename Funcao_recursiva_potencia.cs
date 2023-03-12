dc8c088404c7318a0fbc6aece8ffe68109304431
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-03-11 21:47:09 -0300

    criando_funcao_Exponencial_que_calcula_a_exponenciacao_de_um_numero_base_elevado_a_um_expoente_e_a_recursao_e_utilizada_para_reduzir_o_expoente_ate_que_ele_atinja_o_valor_zero_para_que_seja_calculado_o_resultado_final

---
+++ Funcao_recursiva_potencia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Funcao_recursiva_potencia {     // Função que calcula a exponenciação de um número base elevado a um expoente utilizando recursão   
+    static int Exponencial(int numBase, int expoente) {
+        if (expoente > 0) {     // se 'expoente' for maior que 0
+            return numBase * Exponencial(numBase, expoente - 1);        // é chamada a função 'Exponencial' novamente passando a base e o expoente reduzido em 1 e multiplicando o resultado pela base
+        } else {        // caso contrário é retornado 1
+            return 1;
+        }
+    }
+
+    static void Main() {
+        int numBase = 2;
+        int expoente = 3;
+        int resultado = Exponencial(numBase, expoente);
+        Console.WriteLine($"{numBase}^{expoente} = {resultado}");
+    }
+}
