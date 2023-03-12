c6468f00ea9cc0cd62e20a63110dfb0aa7ac3ec4
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-03-12 20:49:37 -0300

    implementando_funcao_recursiva_para_imprimir_a_tabela_de_'cubo's_de_1_ate_um_valor_maximo_definido_pelo_usuario_a_funcao_utiliza_recursao_para_chamar_a_si_mesma_com_valores_decrementados_de_x_ate_que_x_seja_igual_a_0

---
+++ Funcao_recursiva_interativa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Funcao_recursiva_interativa {
+    public static void iterativa(int x) {       // função recursiva para imprimir a 'tabela de cubos' de 1 até 'x'
+        if (x > 0) {        // enquanto 'x' for maior que 0 chama novamente a função interativa 'recursividade'
+            iterativa(x - 1);       // chamando a função 'interativa' diminuindo 1
+            int exp = x * x * x;        // calculando o cubo de 'x'
+            Console.WriteLine($"O número {x} ao cubo é: {exp}.");       // imprimindo o resultado 
+        }
+    }
+    
+    public static void Main() {
+        int n = 5;      // definição do valor máximo da tabela de cubos
+        Console.WriteLine($"Tabela de cubos de 1 até {n}:");
+        iterativa(n);       // chamando a função 'interativa' passando 'n' como parâmetro
+    }
+}
