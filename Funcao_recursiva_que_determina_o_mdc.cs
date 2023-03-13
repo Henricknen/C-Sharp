a6d45fd60a5c9c5c991a2ca87c8d688b1039ad5a
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-03-13 01:23:06 -0300

    implementando_da_funcao_recursiva_que_determina_o_maximo_divisor_comum_'mdc'_entre_dois_numeros_inteiros_utilizando_o_algoritmo_de_'Euclides'

---
+++ Funcao_recursiva_que_determina_o_mdc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program {
+    public static int Mdc(int x, int y) {       // função recursiva para calcular o 'MDC' de dois números
+        if (y == 0) {        // se 'y' for 0 o 'mdc' será o 'x'
+            return x;
+        } else {    // caso ao contrario é chamanda a função recursiva "Mdc'
+            return Mdc(y, x % y);
+        }
+    }
+    public static void Main() {
+        int x = 24;     // declando variáveis 'x' e 'y' com valores iniçiais
+        int y = 36;
+        int mdc = Mdc(x, y);         // chamada da função 'Mdc' para calcular o mdc entre 'x' e 'y'
+        Console.WriteLine($"O mdc entre {x} e {y} é: {mdc}");       // imprimindo o resultado
+    }    
+}
