06a2c0addb9b3cfa5d0f42033a410bc2b3fae66a
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-03-16 16:08:18 -0300

    convertendo_um_numero_inteiro_para_sua_representacao_'binaria'_usando_uma_'funcao_recursiva'

---
+++ Conversao_de_um_numero_inteiro_para_a_base_binaria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Conversao_de_um_numero_inteiro_para_a_base_binaria {
+    public static void Main() {
+        int x = 20;
+        Console.Write("A representação binária de " + x + " é: ");
+        conversor(x);       // chama a função conversor para converter o número inteiro x em binário
+    }
+    
+    public static void conversor(int x) {
+        if (x > 0) {        // verifique se o número inteiro é maior que 0
+            conversor(x / 2);        // chama a função novamente com o quociente da divisão por 2
+            Console.Write(x % 2);       // imprime o resto da divisão por 2 na tela
+        }
+    }
+}
