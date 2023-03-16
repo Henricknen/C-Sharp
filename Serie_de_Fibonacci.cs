252b8656a8bc5a1527a40a8028ae2f056d643370
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-03-15 22:00:32 -0300

    _implementando_uma_serie_de_'Fibonacci'_utilizando_uma_funcao_'recursiva'_para_calcular_o_n-esimo_termo_o_usuario_insere_o_numero_do_termo_desejado_e_o_resultado_e_impresso_na_tela

---
+++ Serie_de_Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fibonacci {
+    class Serie_de_Fibonacci {
+        static int FibonacciRecursive(int n) {      // Função recursiva para calcular o n-ésimo termo da série de Fibonacci
+            if (n == 0) {
+                return 0;
+            }
+            else if (n == 1) {
+                return 1;
+            } else {
+                return FibonacciRecursive(n-1) + FibonacciRecursive(n-2);       // Calcula o n-ésimo termo somando os dois termos anteriores
+            }
+        }
+
+        static void Main(string[] args) {
+            Console.Write("Digite o n-ésimo termo da série de Fibonacci que deseja calcular: ");
+            int n = int.Parse(Console.ReadLine());
+            int fib = FibonacciRecursive(n);        // Chama a função FibonacciRecursive para calcular o termo
+            Console.WriteLine("O " + n + "º termo da série de Fibonacci é: " + fib);        // Imprime o resultado
+        }
+    }
+}
